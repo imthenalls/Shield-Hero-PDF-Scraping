90b4b1f2e3e4eb1c0c913b3326783d4fac18f1f4
imthenalls <kevinkkrajewski@gmail.com>
2019-05-02 17:12:17 -0400

    Add files via upload

---
+++ pdf.py
@@ -0,0 +1,34 @@
+
+import pdfkit
+import requests
+import bs4
+import os
+
+path = "/Shield Hero"
+try:  
+    os.mkdir(path)
+except OSError:  
+    print ("Creation of the directory %s failed" % path)
+else:  
+    print ("Successfully created the directory %s " % path)
+    os.chdir(path)
+    print(os.getcwd())
+
+res = requests.get('https://ohanashimi.wordpress.com/tate-no-yuusha/')
+res.raise_for_status()
+pdfthing = bs4.BeautifulSoup(res.text)
+elems = pdfthing.select('a')
+#these are 15 and 401 because thats where 
+#the links for the chapters begin in the list of html <a> tags
+startChapter = 15 
+endChapter = 401
+Shield = "Shield Hero "
+chapter = 'Chapter'
+chapterNum = 1;
+pdf = '.pdf'
+while(i<=16):
+	chapterName = Shield + chapter + " " +str(chapterNum) + pdf
+	print(elems[i].get('href'), " " ,i," ",chapterName)
+	pdfkit.from_url(elems[i].get('href'),chapterName)
+	i += 1
+	chapterNum += 1
